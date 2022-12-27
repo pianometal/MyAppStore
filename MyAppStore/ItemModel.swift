90c7f3d5c1aa8f878aede34919384c31187e0bad
Kyle Lovely <41491421+pianometal@users.noreply.github.com>
2022-12-27 14:09:05 -0500

    Created model

---
+++ MyAppStore/ItemModel.swift
@@ -0,0 +1,28 @@
+//
+//  ItemModel.swift
+//  MyAppStore
+//
+//  Created by Kyle Lovely on 12/27/22.
+//
+
+import Foundation
+
+/// Blueprint `Model` for an Item in our shop
+struct ItemModel {
+    
+    /// Item Name
+    var name: String
+    
+    /// Image
+    var image: String
+    
+    /// Description
+    var description: String
+    
+    /// Price
+    var price: Double
+    
+    /// Add To Cart
+    // TODO: Figure this out
+
+}
