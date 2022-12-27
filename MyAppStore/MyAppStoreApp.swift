563ad76240c868016e9602a014b4823733b8e13a
Kyle Lovely <41491421+pianometal@users.noreply.github.com>
2022-12-27 13:46:40 -0500

    Initial Commit

---
+++ MyAppStore/MyAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppStoreApp.swift
+//  MyAppStore
+//
+//  Created by Kyle Lovely on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct MyAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
